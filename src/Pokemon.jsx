4667b8db49ebd311f74c310c7a8d22000989f841
void-the-droid <bhuwan626282@gmail.com>
2025-01-13 12:54:51 +0545

    rsr

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
-export default function Pokemon() {
-   
- const API = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+ export default function Pokemon() {
+   const [pokemon,setPokemon]=useState([]);
+
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
 
     useEffect(()=>{
         fetchPokemon();
@@ -14,12 +15,19 @@ export default function Pokemon() {
             const res= await fetch(API)
             const data = await res.json();
 
-             console.log(data)
+        const detailedPokemonData =data.results.map(async(curPokemon)=>{
+            const res =await fetch(curPokemon.url)
+            const data = await res.json()
+            return data ;
+        })     
+
+        const detailedResponses =await Promise.all(detailedPokemonData)
+        console.log(detailedResponses)
+        setPokemon(detailedResponses)
+
            } catch (error) {
              console.log(error.message)
            }
-         
-
     }
     
     return (
