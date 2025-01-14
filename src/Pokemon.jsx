d1b4ff40e93e953606775b073044c6c8666d2312
void-the-droid <bhuwan626282@gmail.com>
2025-01-14 07:24:41 +0545

    css part

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,46 +1,49 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import PokemonCard from "./PokemonCard";
 
+export default function Pokemon() {
+  const [pokemon, setPokemon] = useState([]);
 
- export default function Pokemon() {
-   const [pokemon,setPokemon]=useState([]);
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
 
-  const API = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
 
-    useEffect(()=>{
-        fetchPokemon();
-    },[])
-  
- const  fetchPokemon=async()=>{
-        try {
-            const res= await fetch(API)
-            const data = await res.json();
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
 
-        const detailedPokemonData =data.results.map(async(curPokemon)=>{
-            const res =await fetch(curPokemon.url)
-            const data = await res.json()
-            return data ;
-        })     
+      const detailedPokemonData = data.results.map(async (curPokemon) => {
+        const res = await fetch(curPokemon.url);
+        const data = await res.json();
+        return data;
+      });
 
-        const detailedResponses =await Promise.all(detailedPokemonData)
-        console.log(detailedResponses)
-        setPokemon(detailedResponses)
-
-           } catch (error) {
-             console.log(error.message)
-           }
+      const detailedResponses = await Promise.all(detailedPokemonData);
+      console.log(detailedResponses);
+      setPokemon(detailedResponses);
+    } catch (error) {
+      console.log(error.message);
     }
-    
-    return (
-      <div className="container">
-  
-  <h1 className="text-3xl font-bold text-center p-4 text-slate-900">
-      Lets catch Pokemon
-    </h1>
-  
-  
+  };
 
-  
+  return (
+    <div className="container">
+      <header>
+        <h1 className="text-3xl font-bold text-center p-8 text-slate-900">
+          Lets catch Pokemon
+        </h1>
+      </header>
+
+      <div className="grid-container">
+        <ul className="cards">
+            {pokemon.map((curPokemon) => {
+return <PokemonCard key={curPokemon.id} pokemonData={curPokemon}/>
+            })}
+        </ul>
       </div>
-   
-    )
+    </div>
+  );
 }
---
+++ src/PokemonCard.jsx
@@ -0,0 +1,11 @@
+
+export default function PokemonCard({pokemonData}) {
+  return (
+<li className="pokemon-card">
+    {pokemonData.name}
+<figure>
+<img src="" alt="" />
+</figure>
+</li>
+  )
+}
